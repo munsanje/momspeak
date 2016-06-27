a7aededa930982fe9c26fc3b4ae1f30254171333
Munsanje Mweene <mmweene15@gmail.com>
2016-06-27 10:30:46 +0200

    .

--- go-app-witbot.js
+++ go-app-witbot.js
@@ -7,17 +7,17 @@ var JsonApi = vumigo.http.api.JsonApi;
 var SESSION_ID = vumigo.utils.uuid();
 // var VERSION = self.im.config.wit.version;
 
-var converse_probe = function(self, im, token, content) {
+var converse_probe = function(im, token, content) {
   var http = new JsonApi(im, {
     headers: {
       'Authorization': ['Bearer ' + token],
-      'Accept': ['application/vnd.wit.' + self.im.config.wit.version + "+json"],
+      'Accept': ['application/vnd.wit.' + im.config.wit.version + "+json"],
       'Content-Type': ['application/json']
     }
   });
   return http.post('https://api.wit.ai/converse?', {
     params: {
-      v: self.im.config.wit.version, // write method that extracts version
+      v: im.config.wit.version, // write method that extracts version
       session_id: SESSION_ID,
       q: content,
     }
@@ -26,19 +26,19 @@ var converse_probe = function(self, im, token, content) {
 
 go.utils = {
 
-    converse: function(self, im, token, content) {
+    converse: function(im, token, content) {
         resp = {};
         // while(resp.type !== "msg") {
         converse_probe(im, token, content)
               .then(function (results) {  // jshint ignore:line
-                  return self.im.log(results)
+                  return im.log(results)
                         .then(function() {
                             return results;
                         });
               });
             resp = converse_probe(im, token, content)
                   .then(function (results) {  // jshint ignore:line
-                      return self.im.log(results)
+                      return im.log(results)
                             .then(function() {
                                 return results;
                             });
