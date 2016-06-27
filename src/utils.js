aedbd2657ba136671ccd2f1e978d801e9315203a
Munsanje Mweene <mmweene15@gmail.com>
2016-06-27 15:26:41 +0200

    .

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+/* jshint -W041*/ // ignore == to compare null warning
 var _ = require('lodash');
 var vumigo = require('vumigo_v02');
 var JsonApi = vumigo.http.api.JsonApi;
@@ -12,13 +13,21 @@ var converse_probe = function(im, token, content) {
       'Content-Type': ['application/json']
     }
   });
-    var resp = http.post('https://api.wit.ai/converse?', {
-            params: {
-              v: im.config.wit.version, // write method that extracts version
-              session_id: SESSION_ID,
-              q: content == null ? undefined : content // jshint ignore:line
+    var resp = http.post('https://api.wit.ai/converse?', content == null ?
+            {
+              params: {
+                v: im.config.wit.version, // write method that extracts version
+                session_id: SESSION_ID,
+                q: content == null ? undefined : content // jshint ignore:line
+              }
             }
-        })
+          : {
+                params: {
+                  v: im.config.wit.version, // write method that extracts version
+                  session_id: SESSION_ID,
+                }
+            }
+        )
         .then(function(response) {
             if(response.type == 'merge') {
                 return converse_probe(im, token, null);
